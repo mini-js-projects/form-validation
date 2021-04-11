51d4a84a9f779ee26bd214243a054a980297a2de
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-11 19:52:55 +0500

    create isValidEmail function to check if email is valid with regex

--- script.js
+++ script.js
@@ -6,15 +6,20 @@ const confirmPassword = document.getElementById('confirmPassword');
 
 function showError(input, message) {
   const formControl = input.parentElement;
-  const small = formControl.lastElementChild;
-  if (input.value === '') {
-    formControl.classList.add('error');
-    small.textContent = message;
-  }
+  const small = formControl.querySelector('small');
+
+  formControl.className = 'form-control error';
+  small.textContent = message;
 }
 
 function showSuccess(input) {
-  input.parentElement.classList.add('success');
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+function isValidEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 // Event Listeners
@@ -25,5 +30,23 @@ form.addEventListener('submit', (e) => {
     showError(username, 'Username is required');
   } else {
     showSuccess(username);
+  }
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else if (!isValidEmail(email.value)) {
+    showError(email, 'Email is not valid');
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+  if (confirmPassword.value === '') {
+    showError(confirmPassword, 'Password must be the same');
+  } else {
+    showSuccess(confirmPassword);
   }
 });
